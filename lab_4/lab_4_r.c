34211fa92841a78a34b1b879664c6596d2cfb951
DIEMVS <sataev.dima@mail.ru>
2020-12-28 19:04:22 +0300

    final fix

--- lab_4/lab_4_r.c
+++ lab_4/lab_4_r.c
@@ -60,12 +60,19 @@ int main(){
 	for(; ; ){
 		struct Value myValue;
 		
+		int semSigRes = semop(semID, &semsignal, 1);
+		if(semSigRes == -1){
+			printf("Ошибка открытия SEMAPHORE\n");
+			exit(EXIT_FAILURE);
+		}	
+		
 		myValue.pid = getpid();
 		strcpy( myValue.str, getTime());
 		printf("PID: %d, Время: %s \n", myValue.pid, myValue.str);
 		printf("Полученные PID: %d, Время: %s \n", recivedValue->pid, recivedValue->str);
-		int semSigRes = semop(semID, &semsignal, 1);
-		if(semSigRes == -1){
+		
+		int semSigWait = semop(semID, &semwait, 1);
+		if(semSigWait == -1){
 			printf("Ошибка открытия SEMAPHORE\n");
 			exit(EXIT_FAILURE);
 		}	
